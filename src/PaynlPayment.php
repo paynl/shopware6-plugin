6a8a791d0db0eb97a31d9f06fbe6b6d4a7d4a69d
Oleh Hinchak <ohinchak@sharpminds.com>
2019-10-31 18:00:10 +0200

    Initial commit

--- src/PaynlPayment.php
+++ src/PaynlPayment.php
@@ -1,94 +1,9 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace Swag\PaynlPayment;
+namespace PaynlPayment;
 
-use Swag\PaynlPayment\Service\ExamplePayment;
-use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin;
-use Shopware\Core\Framework\Plugin\Context\ActivateContext;
-use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
-use Shopware\Core\Framework\Plugin\Context\InstallContext;
-use Shopware\Core\Framework\Plugin\Context\UninstallContext;
-use Shopware\Core\Framework\Plugin\Util\PluginIdProvider;
 
 class PaynlPayment extends Plugin
 {
-    public function install(InstallContext $context): void
-    {
-        $this->addPaymentMethod($context->getContext());
-    }
-
-    public function uninstall(UninstallContext $context): void
-    {
-        // Only set the payment method to inactive when uninstalling. Removing the payment method would
-        // cause data consistency issues, since the payment method might have been used in several orders
-        $this->setPaymentMethodIsActive(false, $context->getContext());
-    }
-
-    public function activate(ActivateContext $context): void
-    {
-        $this->setPaymentMethodIsActive(true, $context->getContext());
-        parent::activate($context);
-    }
-
-    public function deactivate(DeactivateContext $context): void
-    {
-        $this->setPaymentMethodIsActive(false, $context->getContext());
-        parent::deactivate($context);
-    }
-
-    private function addPaymentMethod(Context $context): void
-    {
-        $paymentMethodExists = $this->getPaymentMethodId();
-        // Payment method exists already, no need to continue here
-        if ($paymentMethodExists) {
-            return;
-        }
-
-        /** @var PluginIdProvider $pluginIdProvider */
-        $pluginIdProvider = $this->container->get(PluginIdProvider::class);
-        $pluginId = $pluginIdProvider->getPluginIdByBaseClass($this->getClassName(), $context);
-        $examplePaymentData = [
-            // payment handler will be selected by the identifier
-            'handlerIdentifier' => ExamplePayment::class,
-            'name' => 'Paynl payment',
-            'description' => 'Paynl payment description',
-            'pluginId' => $pluginId,
-        ];
-        /** @var EntityRepositoryInterface $paymentRepository */
-        $paymentRepository = $this->container->get('payment_method.repository');
-        $paymentRepository->create([$examplePaymentData], $context);
-    }
-
-    private function setPaymentMethodIsActive(bool $active, Context $context): void
-    {
-        /** @var EntityRepositoryInterface $paymentRepository */
-        $paymentRepository = $this->container->get('payment_method.repository');
-        $paymentMethodId = $this->getPaymentMethodId();
-        // Payment does not even exist, so nothing to (de-)activate here
-        if (!$paymentMethodId) {
-            return;
-        }
-        $paymentMethod = [
-            'id' => $paymentMethodId,
-            'active' => $active,
-        ];
-        $paymentRepository->update([$paymentMethod], $context);
-    }
-
-    private function getPaymentMethodId(): ?string
-    {
-        /** @var EntityRepositoryInterface $paymentRepository */
-        $paymentRepository = $this->container->get('payment_method.repository');
-        // Fetch ID for update
-        $paymentCriteria = (new Criteria())->addFilter(new EqualsFilter('handlerIdentifier', ExamplePayment::class));
-        $paymentIds = $paymentRepository->searchIds($paymentCriteria, Context::createDefaultContext());
-        if ($paymentIds->getTotal() === 0) {
-            return null;
-        }
-        return $paymentIds->getIds()[0];
-    }
 }
