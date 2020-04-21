08e431e6fd104f1d70bd106ecb5e2f1db7315bc7
poncho-jim <alfonso@pay.nl>
2020-04-21 17:30:50 +0200

    changed JS & CSS to shopware 6 standards

--- src/Resources/app/storefront/src/example-plugin/example-plugin.plugin.js
+++ src/Resources/app/storefront/src/example-plugin/example-plugin.plugin.js
@@ -6,48 +6,15 @@ export default class ExamplePlugin extends Plugin {
          * Specifies the text that is prompted to the user
          * @type string
          */
-        text: 'just some ridiculous text.',
+        text: 'seems like there\'s nothing more to see here.',
     };
 
     init() {
-        document.getElementById('cocField').onchange = (select) => {
-            const dropText = select.options[select.selectedIndex].text;
-            const showField = document.getElementById('cocField');
-            const nl = 'Netherlands';
-            const be = 'Belgium';
-
-            if (dropText === nl || dropText === be){
-                showField.style.display = 'block';
-            } else {
-                showField.style.display = 'none';
+        const that = this;
+        window.onscroll = function() {
+            if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+                alert(that.options.text);
             }
-        }
-
-        /*window.onclick = function idealBanks(displayBanks) {
-                const getBanks = document.getElementById('banks')
-                if (displayBanks === 'block') {
-                    getBanks.style.display = displayBanks;
-                } else {
-                    getBanks.style.display = displayBanks;
-                }
-            }
-
-            const divState = {};
-
-            window.onclick = function showhide(id) {
-                if (document.getElementById) {
-                    const divid = document.getElementById(id);
-                    divState[id] = (!divState[id]);
-                    //close others
-                    for (const div in divState) {
-                        if (divState[div] && div !== id) {
-                            document.getElementById(div).style.display = 'none';
-                            divState[div] = false;
-                        }
-                    }
-                    divid.style.display = (divid.style.display === 'block' ? 'none' : 'block');
-                }
-            }*/
-
+        };
     }
 }
--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,6 +1,10 @@
 // Import all necessary Storefront plugins and scss files
 import ExamplePlugin from './example-plugin/example-plugin.plugin';
+import PaynlCocPlugin from './paynl-cocfield-plugin/paynl-cocfield.plugin';
+import PaynlSavebtnPlugin from './paynl-cocfield-plugin/paynl-cocfield.plugin';
 
 // Register them via the existing PluginManager
 const PluginManager = window.PluginManager;
 PluginManager.register('ExamplePlugin', ExamplePlugin, '[data-example-plugin]');
+PluginManager.register('PaynlCocPlugin', PaynlCocPlugin, '[paynl-coc-plugin]');
+PluginManager.register('PaynlSavebtnPlugin', PaynlSavebtnPlugin, '[paynl-savebtn-plugin]');
