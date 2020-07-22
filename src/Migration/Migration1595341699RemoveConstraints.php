de541cc4298df9987e1852886e43cc8bfd6f7742
Dima Hakman <dhakman@sharpminds.com>
2020-07-22 15:24:50 +0300

    Removed foreign keys from paynl_transactions

---
+++ src/Migration/Migration1595341699RemoveConstraints.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace PaynlPayment\Shopware6\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1595341699RemoveConstraints extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1595341699;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $query = '
+            ALTER TABLE `paynl_transactions`
+            DROP FOREIGN key `fk.paynl_transaction.customer_id`,
+            DROP INDEX `fk.paynl_transaction.customer_id`,
+            DROP FOREIGN key `fk.paynl_transaction.order_id`,
+            DROP INDEX `fk.paynl_transaction.order_id`,
+            DROP FOREIGN key `fk.paynl_transaction.order_state_id`,
+            DROP INDEX `fk.paynl_transaction.order_state_id`;';
+
+        $connection->executeQuery($query);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
