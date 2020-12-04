a92e3443c1cd7316ab8ff8aaee34e7d39fb3b6bb
Ihor Moskalyk <imoskalyk@sharpminds.com>
2020-12-04 14:59:19 +0200

    fixed displaying kvk field on account type change

--- src/Resources/app/storefront/src/paynl-payment-plugin/paynl-kvk-coc-field-toggle.plugin.js
+++ src/Resources/app/storefront/src/paynl-payment-plugin/paynl-kvk-coc-field-toggle.plugin.js
@@ -2,18 +2,58 @@ import Plugin from 'src/plugin-system/plugin.class';
 
 export default class PaynlKvkCocFieldTogglePlugin extends Plugin {
     init() {
-        const countrySelect = document.getElementsByClassName('paynl-country-select');
-        for (let i = 0; i < countrySelect.length; i++) {
-            countrySelect[i].onchange = this.onCountryChangeCallback;
+        const countrySelect = document.querySelector('.paynl-country-select');
+        if (countrySelect !== null) {
+            countrySelect.onchange = this.onCountryChange;
+        }
+
+        const accountTypeSelect = document.querySelector('#addressaccountType');
+        if (accountTypeSelect !== null) {
+            accountTypeSelect.onchange = this.onAccountTypeChange;
         }
     }
 
-    onCountryChangeCallback(event) {
-        const selectedCountryKvkCocField = event.target.selectedOptions[0].dataset.paynlKvkCocField;
+    onCountryChange(event) {
+        const selectedOption = event.target.options[event.target.selectedIndex];
         const kvkCocFieldBlock = document.getElementById('paynl-kvk-coc-number-field');
-        kvkCocFieldBlock.style.display = 'none';
-        if (selectedCountryKvkCocField === 'true') {
-            kvkCocFieldBlock.style.display = 'inline-block';
+
+        if (selectedOption.getAttribute('data-paynl-kvk-coc-field') === null) {
+            kvkCocFieldBlock.style.display = 'none';
+
+            return;
         }
+
+        const accountTypeSelect = document.querySelector('#addressaccountType');
+        if (accountTypeSelect === null || accountTypeSelect.value !== 'business') {
+            kvkCocFieldBlock.style.display = 'none';
+
+            return;
+        }
+
+        kvkCocFieldBlock.style.display = 'inline-block';
+    }
+
+    onAccountTypeChange(event) {
+        const kvkCocFieldBlock = document.getElementById('paynl-kvk-coc-number-field');
+        if (event.target.value !== 'business') {
+            kvkCocFieldBlock.style.display = 'none';
+
+            return;
+        }
+
+        const countrySelect = document.querySelector('.paynl-country-select');
+
+        if (countrySelect === null) {
+            return;
+        }
+        const selectedValue = countrySelect.options[countrySelect.selectedIndex];
+
+        if (selectedValue.getAttribute('data-paynl-kvk-coc-field') === null) {
+            kvkCocFieldBlock.style.display = 'none';
+
+            return;
+        }
+
+        kvkCocFieldBlock.style.display = 'inline-block';
     }
 }
