61d4b3a77abdd92328dcac01829f188a8b0d5ed8
VolodymyrKupchanko <vkupchanko@sharpminds.com>
2021-04-05 23:20:54 +0300

    Kvk/Coc field reworked

--- src/Resources/app/storefront/src/paynl-payment-plugin/paynl-kvk-coc-field-toggle.plugin.js
+++ src/Resources/app/storefront/src/paynl-payment-plugin/paynl-kvk-coc-field-toggle.plugin.js
@@ -13,15 +13,31 @@ export default class PaynlKvkCocFieldTogglePlugin extends Plugin {
     accountFormChange(event) {
         const el = event.target;
         const form = el.form;
+        if (form.getAttribute('id') === 'profilePersonalForm' && el.id === 'accountType') {
+            selectedValues.isBusiness = Boolean(el.value === 'business');
+            selectedValues.isPayCountrySelected = selectedValues.isBusiness;
+            this.toggleCocField(selectedValues, form);
+            return;
+        }
 
+        if (!form.classList.contains('register-form')) {
+            return;
+        }
+
+        const kvkBlock = form.querySelector('.paynl-kvk-coc-number-field');
+        const kvkCountriesIds = kvkBlock.getAttribute('data-countries-with-kvk').split(',');
         if (el.id === 'accountType' || el.id === 'addressaccountType') {
             selectedValues.isBusiness = Boolean(el.value === 'business');
-            const countrySelect = form.querySelector('.paynl-country-select');
+            const countrySelect = form.querySelector('.country-select');
             if (countrySelect !== null) {
-                selectedValues.isPayCountrySelected = Boolean(countrySelect.options[countrySelect.selectedIndex].getAttribute('data-paynl-kvk-coc-field'));
+                selectedValues.isPayCountrySelected = Boolean(
+                    kvkCountriesIds.includes(countrySelect.options[countrySelect.selectedIndex].getAttribute('value'))
+                );
             }
-        } else if (el.classList.contains('paynl-country-select')) {
-            selectedValues.isPayCountrySelected = Boolean(el.options[el.selectedIndex].getAttribute('data-paynl-kvk-coc-field'));
+        } else if (el.classList.contains('country-select')) {
+            selectedValues.isPayCountrySelected = Boolean(
+                kvkCountriesIds.includes(el.options[el.selectedIndex].getAttribute('value'))
+            );
             const accountTypeSelect = form.querySelector('select[name="address[accountType]"]');
             if (accountTypeSelect !== null) {
                 selectedValues.isBusiness = Boolean(accountTypeSelect.value === 'business');
--- src/Subscriber/CustomerRegisterSubscriber.php
+++ src/Subscriber/CustomerRegisterSubscriber.php
@@ -5,9 +5,11 @@ namespace PaynlPayment\Shopware6\Subscriber;
 use PaynlPayment\Shopware6\Helper\CustomerHelper;
 use Shopware\Core\Checkout\Customer\Aggregate\CustomerAddress\CustomerAddressEntity;
 use Shopware\Core\Checkout\Customer\CustomerEvents;
+use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -45,6 +47,7 @@ class CustomerRegisterSubscriber implements EventSubscriberInterface
     {
         return [
             CustomerEvents::CUSTOMER_ADDRESS_WRITTEN_EVENT => 'onCustomerAddressChanged',
+            CustomerEvents::CUSTOMER_WRITTEN_EVENT => 'onCustomerProfileChanged',
         ];
     }
 
@@ -56,17 +59,40 @@ class CustomerRegisterSubscriber implements EventSubscriberInterface
             $cocNumber = $request->get('coc_number');
             $addressIdArray = $event->getIds();
             $context = $event->getContext();
+            $criteria = new Criteria($addressIdArray);
 
-            /** @var CustomerAddressEntity $customerAddress */
-            $customerAddress = $this->customerAddressRepository->search(new Criteria($addressIdArray), $context)->first();
-
-            if ($customerAddress !== null) {
-                if(!is_null($cocNumber) && ($customerAddress instanceof CustomerAddressEntity)) {
-                    $this->customerHelper->saveCocNumber($customerAddress, $cocNumber, $event->getContext());
-                }
-            }
+            $this->updateCustomerCocNumber($cocNumber, $criteria, $context);
         }
 
         return;
+    }
+
+    public function onCustomerProfileChanged(EntityWrittenEvent $event)
+    {
+        if ($this->counter < 1) {
+            $this->counter++;
+            $request = $this->requestStack->getMasterRequest();
+            $cocNumber = $request->get('coc_number');
+            $context = $event->getContext();
+            $criteria = new Criteria();
+            $payloads = $event->getPayloads();
+            $criteria->addFilter(new EqualsFilter('customerId', $payloads[0]['id']));
+
+            $this->updateCustomerCocNumber($cocNumber, $criteria, $context);
+        }
+
+        return;
+    }
+
+    private function updateCustomerCocNumber(?string $cocNumber, Criteria $criteria, Context $context)
+    {
+        /** @var CustomerAddressEntity $customerAddress */
+        $customerAddress = $this->customerAddressRepository->search($criteria, $context)->first();
+
+        if ($customerAddress !== null) {
+            if(!is_null($cocNumber) && ($customerAddress instanceof CustomerAddressEntity)) {
+                $this->customerHelper->saveCocNumber($customerAddress, $cocNumber, $context);
+            }
+        }
     }
 }
