69efcc9b83ce1717896d41877db9c0edc4b258cb
Stas <szhalba@sharpminds.com>
2021-08-05 14:12:24 +0300

    Fixed some bugs

--- src/Subscriber/PaymentMethodIssuerSubscriber.php
+++ src/Subscriber/PaymentMethodIssuerSubscriber.php
@@ -3,9 +3,14 @@
 namespace PaynlPayment\Shopware6\Subscriber;
 
 use PaynlPayment\Shopware6\Helper\CustomerHelper;
+use PaynlPayment\Shopware6\Service\PaymentMethodCustomFields;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Customer\Event\CustomerChangedPaymentMethodEvent;
+use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Shopware\Core\System\SalesChannel\Event\SalesChannelContextSwitchEvent;
@@ -15,14 +20,19 @@ class PaymentMethodIssuerSubscriber implements EventSubscriberInterface
     /** @var Session $session */
     private $session;
 
+    /** @var EntityRepositoryInterface */
+    private $paymentMethodRepository;
+
     /** @var CustomerHelper $customerHelper */
     private $customerHelper;
 
     public function __construct(
         Session $session,
+        EntityRepositoryInterface $paymentMethodRepository,
         CustomerHelper $customerHelper
     ) {
         $this->session = $session;
+        $this->paymentMethodRepository = $paymentMethodRepository;
         $this->customerHelper = $customerHelper;
     }
 
@@ -48,14 +58,8 @@ class PaymentMethodIssuerSubscriber implements EventSubscriberInterface
         }
         $requestDataBagArray = $event->getRequestDataBag()->all();
         $customer = $event->getSalesChannelContext()->getCustomer();
-        $context = $event->getContext();
 
         $this->processPayLaterFields($requestDataBagArray, $customer, $event->getContext());
-
-        $paymentMethodId = (string)$requestDataBagArray['paymentMethodId'];
-        $paynlIssuer = (string)($requestDataBagArray['paynlIssuer'] ?? '');
-
-        $this->customerHelper->savePaynlIssuer($customer, $paymentMethodId, $paynlIssuer, $context);
     }
 
     /**
@@ -90,5 +94,32 @@ class PaymentMethodIssuerSubscriber implements EventSubscriberInterface
         if ($dob) {
             $this->customerHelper->saveCustomerBirthdate($customer, $dob, $context);
         }
+
+        $this->savePaynlPaymentMethodIssuer($requestData, $customer, $context);
+    }
+
+    private function savePaynlPaymentMethodIssuer(array $requestData, CustomerEntity $customer, Context $context): void
+    {
+        /** @var PaymentMethodEntity $paymentMethod */
+        $paymentMethod = $this->paymentMethodRepository->search(
+            (new Criteria())
+                ->addFilter(new EqualsFilter('id', $requestData['paymentMethodId'])),
+            $context
+        )->first();
+
+        if (empty($paymentMethod)) {
+            return;
+        }
+
+        $paymentMethodCustomFields = $paymentMethod->getCustomFields();
+        $paymentMethodDisplayBanks = $paymentMethodCustomFields[PaymentMethodCustomFields::DISPLAY_BANKS_FIELD] ?? null;
+        if (!$paymentMethodDisplayBanks) {
+            return;
+        }
+
+        $paymentMethodId = (string)$requestData['paymentMethodId'];
+        $paynlIssuer = (string)($requestData['paynlIssuer'] ?? '');
+
+        $this->customerHelper->savePaynlIssuer($customer, $paymentMethodId, $paynlIssuer, $context);
     }
 }
