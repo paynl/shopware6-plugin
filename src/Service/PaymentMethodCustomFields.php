a3b9ff2822d386018c703bc4e79df1a2f344f59a
Stas <szhalba@sharpminds.com>
2021-09-17 15:43:00 +0300

    Payment banks selecting functionality was updated

--- src/Service/PaymentMethodCustomFields.php
+++ src/Service/PaymentMethodCustomFields.php
@@ -2,6 +2,7 @@
 
 namespace PaynlPayment\Shopware6\Service;
 
+use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
 use Shopware\Storefront\Page\PageLoadedEvent;
 
@@ -48,8 +49,41 @@ class PaymentMethodCustomFields
         $isPaymentDisplayBanks = $this->getCustomField(self::DISPLAY_BANKS_FIELD);
         $isPaymentPayLater = $this->getCustomField(self::IS_PAY_LATER_FIELD);
         $hasPaymentLaterInputs = $isPaymentPayLater && (($isBirthdayExists && $isPhoneNumberExists) === false);
+        $selectedBank = $this->getPaymentMethodSelectedBank($paymentMethod, $event->getSalesChannelContext()->getCustomer());
+        $hasAdditionalInfoInput = ($isPaymentDisplayBanks && is_null($selectedBank)) || $hasPaymentLaterInputs;
 
-        $hasAdditionalInfoInput = $isPaymentDisplayBanks || $hasPaymentLaterInputs;
         $this->setCustomField(self::HAS_ADDITIONAL_INFO_INPUT_FIELD, $hasAdditionalInfoInput);
+    }
+
+    private function getPaymentMethodSelectedBank(PaymentMethodEntity $paymentMethod, CustomerEntity $customer): ?array
+    {
+        $paymentMethodCustomFields = $paymentMethod->getTranslation('customFields');
+        if (!isset($paymentMethodCustomFields['displayBanks']) || $paymentMethodCustomFields['displayBanks'] == false) {
+            return null;
+        }
+
+        $customerCustomFields = $customer->getCustomFields();
+        if (!isset($customerCustomFields['paymentMethodsSelectedData'])) {
+            return null;
+        }
+
+        $paymentMethodsSelectedData = $customerCustomFields['paymentMethodsSelectedData'];
+
+        if (!isset($paymentMethodsSelectedData[$paymentMethod->getId()])) {
+            return null;
+        }
+
+        $issuer = $paymentMethodsSelectedData[$paymentMethod->getId()]['issuer'] ?? null;
+
+        if (is_null($issuer)) {
+            return null;
+        }
+
+        $banks = $paymentMethodCustomFields['banks'];
+        $selectedBank = array_filter($banks, function ($bank) use ($issuer) {
+            return $bank['id'] == $issuer;
+        });
+
+        return current($selectedBank);
     }
 }
