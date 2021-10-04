b2fec172c83ca03e5a217213332375ae5204bab0
Stas <szhalba@sharpminds.com>
2021-10-04 16:05:48 +0300

    The save button was hidden for the IDEAL payment method

--- src/Service/PaymentMethodCustomFields.php
+++ src/Service/PaymentMethodCustomFields.php
@@ -2,7 +2,6 @@
 
 namespace PaynlPayment\Shopware6\Service;
 
-use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
 use Shopware\Storefront\Page\PageLoadedEvent;
 
@@ -46,44 +45,9 @@ class PaymentMethodCustomFields
         $isBirthdayExists = $pageData['isBirthdayExists'] ?? true;
         $isPhoneNumberExists = $pageData['isPhoneNumberExists'] ?? true;
 
-        $isPaymentDisplayBanks = $this->getCustomField(self::DISPLAY_BANKS_FIELD);
         $isPaymentPayLater = $this->getCustomField(self::IS_PAY_LATER_FIELD);
         $hasPaymentLaterInputs = $isPaymentPayLater && (($isBirthdayExists && $isPhoneNumberExists) === false);
-        $selectedBank = $this->getPaymentMethodSelectedBank($paymentMethod, $event->getSalesChannelContext()->getCustomer());
-        $hasAdditionalInfoInput = ($isPaymentDisplayBanks && is_null($selectedBank)) || $hasPaymentLaterInputs;
 
-        $this->setCustomField(self::HAS_ADDITIONAL_INFO_INPUT_FIELD, $hasAdditionalInfoInput);
-    }
-
-    private function getPaymentMethodSelectedBank(PaymentMethodEntity $paymentMethod, CustomerEntity $customer): ?array
-    {
-        $paymentMethodCustomFields = $paymentMethod->getTranslation('customFields');
-        if (!isset($paymentMethodCustomFields['displayBanks']) || $paymentMethodCustomFields['displayBanks'] == false) {
-            return null;
-        }
-
-        $customerCustomFields = $customer->getCustomFields();
-        if (!isset($customerCustomFields['paymentMethodsSelectedData'])) {
-            return null;
-        }
-
-        $paymentMethodsSelectedData = $customerCustomFields['paymentMethodsSelectedData'];
-
-        if (!isset($paymentMethodsSelectedData[$paymentMethod->getId()])) {
-            return null;
-        }
-
-        $issuer = $paymentMethodsSelectedData[$paymentMethod->getId()]['issuer'] ?? null;
-
-        if (is_null($issuer)) {
-            return null;
-        }
-
-        $banks = $paymentMethodCustomFields['banks'];
-        $selectedBank = array_filter($banks, function ($bank) use ($issuer) {
-            return $bank['id'] == $issuer;
-        });
-
-        return current($selectedBank);
+        $this->setCustomField(self::HAS_ADDITIONAL_INFO_INPUT_FIELD, $hasPaymentLaterInputs);
     }
 }
