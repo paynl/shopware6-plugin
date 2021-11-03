25e21a069ac8b413f9af2de767c344965db6a5d5
Stas <szhalba@sharpminds.com>
2021-11-03 11:38:48 +0200

    The notify message was changed

--- src/Helper/ProcessingHelper.php
+++ src/Helper/ProcessingHelper.php
@@ -100,7 +100,8 @@ class ProcessingHelper
         if ($this->checkDoubleOrderTransactions($paynlTransactionEntity, Context::createDefaultContext())) {
             $this->updateOldPaynlTransactionStatus($paynlTransactionEntity, $paynlApiTransaction);
 
-            return sprintf("TRUE| Transaction wasn't processed because there are newer ones transactions. \n" .
+            return sprintf("TRUE| " .
+                "The current transaction is canceled, commands are ignored due to a later completed transaction. \n" .
                 "OrderId: %s",
                 $paynlTransactionEntity->getOrder()->getOrderNumber()
             );
