a860bb2d58b8abf1b29084fde5720b188ad205bf
Stas <szhalba@sharpminds.com>
2023-02-22 18:41:00 +0200

    Fixed ideal and pin payment on order edit page

---
+++ src/Helper/RequestDataBagHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PaynlPayment\Shopware6\Helper;
+
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestDataBagHelper
+{
+    /**
+     * On the edit order page, we don't get a correct DataBag with the issuer data. Therefore we need to get this
+     * data from the $_POST/$_GET.
+     */
+    public function getDataBagItem(string $name, RequestDataBag $dataBag)
+    {
+        if ($dataBag->get($name)) {
+            return $dataBag->get($name);
+        }
+
+        $request = (new Request($_GET, $_POST, array(), $_COOKIE, $_FILES, $_SERVER))->request;
+        return $request->get($name);
+    }
+}
