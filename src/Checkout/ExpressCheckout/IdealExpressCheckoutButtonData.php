5c3da1ae13b19e4e734a692da12c2a5f38e52036
Stas <szhalba@sharpminds.com>
2024-11-07 11:33:40 +0200

    IDeal express checkout data configuration

---
+++ src/Checkout/ExpressCheckout/IdealExpressCheckoutButtonData.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace PaynlPayment\Shopware6\Checkout\ExpressCheckout;
+
+use Shopware\Core\Framework\Struct\Struct;
+
+class IdealExpressCheckoutButtonData extends Struct
+{
+    protected bool $expressCheckoutEnabled;
+    protected bool $expressShoppingCartEnabled;
+    protected ?string $paymentMethodId = null;
+
+    /**
+     * @return bool
+     */
+    public function isExpressCheckoutEnabled(): bool
+    {
+        return $this->expressCheckoutEnabled;
+    }
+
+    /**
+     * @param bool $expressCheckoutEnabled
+     */
+    public function setExpressCheckoutEnabled(bool $expressCheckoutEnabled): void
+    {
+        $this->expressCheckoutEnabled = $expressCheckoutEnabled;
+    }
+
+    public function isExpressShoppingCartEnabled(): bool
+    {
+        return $this->expressShoppingCartEnabled;
+    }
+
+    public function setExpressShoppingCartEnabled(bool $expressShoppingCartEnabled): void
+    {
+        $this->expressShoppingCartEnabled = $expressShoppingCartEnabled;
+    }
+}
--- src/Checkout/ExpressCheckout/Service/ExpressCheckoutDataService.php
+++ src/Checkout/ExpressCheckout/Service/ExpressCheckoutDataService.php
@@ -2,21 +2,28 @@
 
 namespace PaynlPayment\Shopware6\Checkout\ExpressCheckout\Service;
 
-use PaynlPayment\Shopware6\Checkout\ExpressCheckout\ExpressCheckoutButtonData;
+use PaynlPayment\Shopware6\Checkout\ExpressCheckout\PayPalExpressCheckoutButtonData;
+use PaynlPayment\Shopware6\Checkout\ExpressCheckout\IdealExpressCheckoutButtonData;
 use PaynlPayment\Shopware6\Components\Config;
+use PaynlPayment\Shopware6\Entity\PaynlTransactionEntity;
 use PaynlPayment\Shopware6\Helper\LocaleCodeHelper;
+use PaynlPayment\Shopware6\Repository\OrderCustomer\OrderCustomerRepository;
 use PaynlPayment\Shopware6\Repository\PaymentMethod\PaymentMethodRepository;
+use PaynlPayment\Shopware6\Repository\PaynlTransactions\PaynlTransactionsRepository;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\Routing\RouterInterface;
 
-class PayPalExpressCheckoutDataService implements ExpressCheckoutDataServiceInterface
+class ExpressCheckoutDataService implements ExpressCheckoutDataServiceInterface
 {
     private RouterInterface $router;
     private Config $config;
     private LocaleCodeHelper $localeCodeHelper;
     private PaymentMethodRepository $paymentMethodRepository;
+    private OrderCustomerRepository $orderCustomerRepository;
 
     /**
      * @internal
@@ -25,28 +32,31 @@ class PayPalExpressCheckoutDataService implements ExpressCheckoutDataServiceInte
         RouterInterface $router,
         Config $config,
         LocaleCodeHelper $localeCodeHelper,
-        PaymentMethodRepository $paymentMethodRepository
+        PaymentMethodRepository $paymentMethodRepository,
+        OrderCustomerRepository $orderCustomerRepository,
     ) {
         $this->router = $router;
         $this->config = $config;
         $this->localeCodeHelper = $localeCodeHelper;
         $this->paymentMethodRepository = $paymentMethodRepository;
+        $this->orderCustomerRepository = $orderCustomerRepository;
     }
 
-    public function buildExpressCheckoutButtonData(
+    public function buildPayPalExpressCheckoutButtonData(
         SalesChannelContext $salesChannelContext,
         bool $addProductToCart = false
-    ): ?ExpressCheckoutButtonData {
+    ): ?PayPalExpressCheckoutButtonData {
         $context = $salesChannelContext->getContext();
         $salesChannelId = $salesChannelContext->getSalesChannelId();
         $customer = $salesChannelContext->getCustomer();
         $loggedInCustomerEnabled = $this->config->getPaymentPayPalExpressLoggedInCustomerEnabled($salesChannelId);
+        $this->isCompletedCustomerOrder($salesChannelContext);
 
         if (!$loggedInCustomerEnabled && $customer instanceof CustomerEntity && $customer->getActive()) {
             return null;
         }
 
-        return (new ExpressCheckoutButtonData())->assign([
+        return (new PayPalExpressCheckoutButtonData())->assign([
             'expressCheckoutEnabled' => $this->config->getPaymentPayPalExpressCheckoutEnabled($salesChannelId),
             'expressShoppingCartEnabled' => $this->config->getPaymentPayPalExpressShoppingCartEnabled($salesChannelId),
             'clientId' => $this->config->getPaymentPayPalClientIdSandbox($salesChannelId),
@@ -66,6 +76,23 @@ class PayPalExpressCheckoutDataService implements ExpressCheckoutDataServiceInte
         ]);
     }
 
+    public function buildIdealExpressCheckoutButtonData(SalesChannelContext $salesChannelContext, bool $addProductToCart = false): ?IdealExpressCheckoutButtonData
+    {
+        $context = $salesChannelContext->getContext();
+        $salesChannelId = $salesChannelContext->getSalesChannelId();
+        $customer = $salesChannelContext->getCustomer();
+        $loggedInCustomerEnabled = $this->config->getPaymentIdealExpressLoggedInCustomerEnabled($salesChannelId);
+
+        if (!$loggedInCustomerEnabled && $customer instanceof CustomerEntity && $customer->getActive()) {
+            return null;
+        }
+
+        return (new IdealExpressCheckoutButtonData())->assign([
+            'expressCheckoutEnabled' => $this->config->getPaymentIdealExpressCheckoutEnabled($salesChannelId),
+            'expressShoppingCartEnabled' => $this->config->getPaymentIdealExpressShoppingCartEnabled($salesChannelId),
+        ]);
+    }
+
     private function getInContextButtonLanguage(Context $context): string
     {
         return str_replace(
@@ -73,5 +100,20 @@ class PayPalExpressCheckoutDataService implements ExpressCheckoutDataServiceInte
             '_',
             $this->localeCodeHelper->getLocaleCodeFromContext($context)
         );
+    }
+
+    private function isCompletedCustomerOrder(SalesChannelContext $salesChannelContext): bool
+    {
+        $customer = $salesChannelContext->getCustomer();
+        if (! $customer) {
+            return true;
+        }
+
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('customer_id', $salesChannelContext->getCustomerId()));
+
+        $orderCustomer = $this->orderCustomerRepository->search($criteria, $salesChannelContext->getContext())->last();
+
+        dd($orderCustomer);
     }
 }
