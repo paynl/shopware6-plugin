7e45cf7d656026534a8ee0b382739b7b556f486a
Stas <szhalba@sharpminds.com>
2024-11-19 17:47:04 +0200

    iDEAL fast payment modal

---
+++ src/Resources/app/storefront/src/paynl-payment-plugin/paynl-fast-checkout-button.plugin.js
@@ -0,0 +1,30 @@
+import Plugin from 'src/plugin-system/plugin.class';
+
+export default class PaynlFastCheckoutButton extends Plugin {
+    init() {
+        console.log('12345 stas');
+        document.getElementById('btn-fast-checkout').addEventListener('click', this.onFastCheckoutClick.bind(this));
+        document.querySelector('.paynl-ideal-modal .button-secondary').addEventListener('click', this.onCloseModalClick.bind(this));
+        document.querySelector('.paynl-ideal-modal-backdrop').addEventListener('click', this.onCloseModalClick.bind(this));
+    }
+
+    onFastCheckoutClick(event) {
+        event.preventDefault();
+
+        const modal = document.querySelector('.paynl-ideal-modal');
+        const backdrop = document.querySelector('.paynl-ideal-modal-backdrop');
+        modal.classList.add('visible');
+        backdrop.classList.add('visible');
+        document.body.style.overflow = 'hidden';
+    }
+
+    onCloseModalClick(event) {
+        event.preventDefault();
+
+        const modal = document.querySelector('.paynl-ideal-modal');
+        const backdrop = document.querySelector('.paynl-ideal-modal-backdrop');
+        modal.classList.remove('visible');
+        backdrop.classList.remove('visible');
+        document.body.style.overflow = '';
+    }
+}
