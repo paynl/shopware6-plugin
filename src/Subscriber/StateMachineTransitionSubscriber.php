692fbf4c6c4616a69ba6191d2c56b1f58ed10bcd
Stas <szhalba@sharpminds.com>
2025-03-03 16:31:58 +0200

    State machine transition events

---
+++ src/Subscriber/StateMachineTransitionSubscriber.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace PaynlPayment\Shopware6\Subscriber;
+
+use PaynlPayment\Shopware6\Components\Config;
+use Psr\Log\LoggerInterface;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransactionCaptureRefund\OrderTransactionCaptureRefundDefinition;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransactionCaptureRefund\OrderTransactionCaptureRefundStates;
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
+use Shopware\Core\System\StateMachine\Event\StateMachineTransitionEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class StateMachineTransitionSubscriber implements EventSubscriberInterface
+{
+    private Config $config;
+    private LoggerInterface $logger;
+
+    public function __construct(Config $config, LoggerInterface $logger)
+    {
+        $this->config = $config;
+        $this->logger = $logger;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            StateMachineTransitionEvent::class => 'stateChanged',
+            'order_transaction_capture_refund.written' => 'lineItemWritten'
+        ];
+    }
+
+    public function lineItemWritten(EntityWrittenEvent $event): void
+    {
+        $this->logger->info('Order transaction capture refund event: ' . $event->getEntityName(), [
+            'event' => $event,
+        ]);
+    }
+
+    public function stateChanged(StateMachineTransitionEvent $event): void
+    {
+        $context = $event->getContext();
+
+        $to = $event->getToPlace()->getTechnicalName();
+        $from = $event->getFromPlace()->getTechnicalName();
+
+        $this->logger->info('State machine transition event: ' . $event->getEntityName(), [
+            'from' => $from,
+            'to' => $to,
+            'event' => $event,
+        ]);
+
+        if ($event->getEntityName() !== OrderTransactionCaptureRefundDefinition::ENTITY_NAME) {
+            return;
+        }
+
+        if ($to !== OrderTransactionCaptureRefundStates::STATE_COMPLETED) {
+            return;
+        }
+    }
+}
