13d66a1f4c7e50050821bc225e1c31fc370ff4ce
Stas Zhalba <stas@pay.nl>
2025-05-06 14:33:10 +0300

    Shopware 6.6 support

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -53,8 +53,13 @@ class CartService implements CartServiceInterface
     public function addProduct(string $productId, int $quantity, SalesChannelContext $context): Cart
     {
         $cart = $this->getCalculatedMainCart($context);
+        $data = [
+            'id' => $productId,
+            'referencedId' => $productId,
+            'quantity' => $quantity
+        ];
 
-        $productItem = $this->productItemFactory->create($productId, ['quantity' => $quantity]);
+        $productItem = $this->productItemFactory->create($data, $context);
 
         return $this->swCartService->add($cart, $productItem, $context);
     }
